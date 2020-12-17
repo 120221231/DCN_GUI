97ebdc16e71af6e26f8e3e9638c110748bf8fb72
Selja <seljatheresgeorge@gmail.com>
2020-12-17 21:58:13 +0000

    JSON Changes

--- gui_backend.js
+++ gui_backend.js
@@ -1,11 +1,17 @@
+window.onload = loadJobStatusData;
+window.onload = loadServerStatusData;
+window.onload = populateJobStatusTable;
+window.onload = populateServerStatusTable;
 const axios = require('axios');
 function sendChaosFormData(){
     var error = false;
-    var chaosRadioButtonConnection = document.getElementById("chaos_radio_connection");
-    var chaosRadioButtonComponent = document.getElementById("chaos_radio_component");
+    var chaosRadioButtonKill = document.getElementById("chaos_radio_kill_component");
+    var chaosRadioButtonDisconnect = document.getElementById("chaos_radio_disconnect_component");
+    var chaosRadioButtonReconnect= document.getElementById("chaos_radio_reconnect_connection");
     var chaosId = document.getElementById("chaos_id");
-    if(!(chaosRadioButtonConnection.checked
-      ||chaosRadioButtonComponent.checked)){
+    if(!(chaosRadioButtonKill.checked
+      ||chaosRadioButtonDisconnect.checked)
+      ||chaosRadioButtonReconnect.checked){
         alert("choose connection/component");
         error = true;
     }
@@ -14,29 +20,29 @@ function sendChaosFormData(){
         error = true;
     }
     if(!error){
-        var dcnNode;
-        if (chaosRadioButtonConnection.checked)
-        dcnNode = "Connection";
-        else
-        dcnNode = "Component";
+        var url;
+        if (chaosRadioButtonKill.checked)
+          url = "http://localhost:5021/kill";
+        else if(chaosRadioButtonDisconnect.checked)
+          url = "http://localhost:5021/kill";
+        else if(chaosRadioButtonReconnect.checked)
+          url = "http://localhost:5021/kill";
         var component_id = chaosId.value;
-        axios({
-          method: 'post',
-          url: 'http://localhost:3030/abc',
-          data: {
-            component_id: component_id
-                }
-        });
+        axios.post(url, {
+            "component_id": component_id
+            })
+            .then(resp => console.log(resp.data))
+            .catch(console.log);
     }
     return error;
 }
 function sendStressTestingFormData(){
     var error = false;
-    var userName = document.getElementById("stress_user_name");
+    var jobId = document.getElementById("stress_job_id");
     var numberOfJobs = document.getElementById("stress_number_of_jobs");
     var jobDuration = document.getElementById("stress_duration_of_job");
     
-    if(userName.value == ""){
+    if(jobId.value == ""){
         alert("enter user name");
         error = true;
     }
@@ -49,7 +55,7 @@ function sendStressTestingFormData(){
         error = true;
     }
     if(!error){
-        var job_name=userName.value;
+        var job_name=jobId.value;
         var num_jobs=numberOfJobs.value;
         var job_duration=jobDuration.value;
         
@@ -66,88 +72,59 @@ function sendStressTestingFormData(){
                 })
             .then(resp => console.log(resp.data))
             .catch(console.log);
-        // axios({
-        //   method: 'post',
-        //   url: 'http://localhost:5011/jobs',
-        //   data: {
-        //     'job_name':job_name,
-        //     'num_jobs':num_jobs,
-        //     'job_duration': job_duration
-        //         }
-        // });
       } 
     return error;
 }
 
+function loadJobStatusData(){
+  const jobsInterval = setInterval(function() {
+    populateJobStatusTable();
+  }, 5000);
+}
+
+function loadServerStatusData(){
+  const ServerStatusInterval = setInterval(function() {
+    populateServerStatusTable();
+    alert("here")
+  }, 10000);
+}
+
 function populateJobStatusTable(){
-  const table = document.getElementById("job_status_table_body");
-  
-  // let row = table.insertRow();
-  // let job = row.insertCell(0);
-  // job.innerHTML = "Job Name";
-  // let completed = row.insertCell(1);
-  // completed.innerHTML = "Completed";
-  // let total = row.insertCell(2);
-  // total.innerHTML = "Total";
+  const table = document.getElementById("job_status_table");
 
-  // monitoringSystemUrl = "http://localhost:5031/job_status";
-  // axios.get(monitoringSystemUrl)
-  //   .then(resp => {
-  //     Object.keys(JSON.parse(resp.data)).forEach(job => {
-  //       let row = table.insertRow();
-  //       let job_row = row.insertCell(0);
-  //       job_row.innerHTML = job;
-  //       let completed = row.insertCell(1);
-  //       completed.innerHTML = resp.json[job]["completed"];
-  //       let total = row.insertCell(2);
-  //       total.innerHTML = resp.json[job]["total"];
-  //     }
-  //   })
-  //   .catch(err => console.log(err));
-
-
-  // row = table.insertRow();
-  // job = row.insertCell(0);
-  // job.innerHTML = "Job 2";
-  // time = row.insertCell(1);
-  // time.innerHTML = "20";
-
-  // modifyJobStatus();
+  monitoringSystemUrl = "http://localhost:5031/job_status";
+  axios.get(monitoringSystemUrl)
+    .then(resp => {
+      Object.keys(JSON.parse(resp.data)).forEach(job => {
+        let row = table.insertRow();
+        let job_cell = row.insertCell(0);
+        job_cell.innerHTML = job;
+        let completed_cell = row.insertCell(1);
+        completed_cell.innerHTML = resp.json[job]["completed"];
+        let total_cell = row.insertCell(2);
+        total_cell.innerHTML = resp.json[job]["total"];
+      })
+    })
+    .catch(err => console.log(err));
 }
 
 function populateServerStatusTable(){
-  const table = document.getElementById("server_status_table_body");
-
-  let row = table.insertRow();
-  let server = row.insertCell(0);
-  let cpu_usage = row.insertCell(1);
-  let ram_usage = row.insertCell(2);
-  server.innerHTML = "server 1";
-  cpu_usage.innerHTML = "10";
-  ram_usage.innerHTML = "20";
-}
-
-function modifyJobStatus(){
-  var table= Array.prototype.slice.call(document.getElementById("job_status_table_body").rows, 0);
-  table.forEach(element => {
-    if(element.cells[0].innerHTML == "Job 1"){
-      element.cells[1].innerHTML = 12;
-    }
-  });
-}
-
-function modifyCPUUsage(){
-  var table = Array.prototype.slice.call(document.getElementById("server_status_table_body").rows, 0);
-  table.forEach(element => {
-    //do something
-  });
-}
-
-function modifyRAMUsage(){
-  var table = Array.prototype.slice.call(document.getElementById("server_status_table_body").rows, 0);
-  table.forEach(element => {
-    //do something
-  });
+  const table = document.getElementById("server_utilization_status_table");
+  
+  monitoringSystemUrl = "http://localhost:5031/status";
+  axios.get(monitoringSystemUrl)
+    .then(resp => {
+      Object.keys(JSON.parse(resp.data)).forEach(component => {
+        let row = table.insertRow();
+        let component_cell = row.insertCell(0);
+        component_cell.innerHTML = component["name"];
+        let cpu_utilization_cell = row.insertCell(1);
+        cpu_utilization_cell.innerHTML = resp.json[component]["stats"]["cpu_percentage"];
+        let ram_utilization_cell = row.insertCell(2);
+        ram_utilization_cell.innerHTML = resp.json[component]["stats"]["mem_percentage"];
+      })
+    })
+    .catch(err => console.log(err));
 }
 
 module.exports = sendStressTestingFormData;
